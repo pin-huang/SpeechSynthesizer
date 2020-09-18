f0f9e9bad4382feab7d8551beda9eeee1a074ec6
Pin Huang <justmailmason@gmail.com>
2020-09-18 18:36:15 +0800

    Initial Commit

---
+++ SpeechSynthesizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechSynthesizer
+//
+//  Created by Pincheng Huang on 2020/9/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
